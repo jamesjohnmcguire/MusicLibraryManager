7becb210a8b5458e08b6b17f7f676b9bae684990
JamesMc <jamesjohnmcguire@gmail.com>
2019-08-21 03:01:38 +0900

    Added Rule class.

---
+++ SourceCode/MusicUtility/Rule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicUtility
+{
+	public class Rule
+	{
+		public object Subject { get; set; }
+
+		public Condition Condition { get; set; }
+
+		public object Conditional { get; set; }
+
+		public Operations Operation { get; set; }
+	}
+}
