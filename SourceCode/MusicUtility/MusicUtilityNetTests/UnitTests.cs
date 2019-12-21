cdd3e4b8ce4ce54e4a753764814c80e2e71b9eb3
JamesMc <jamesjohnmcguire@gmail.com>
2019-12-21 17:47:53 +0900

    Addition of RunRuleBasic test.

--- SourceCode/MusicUtility/MusicUtilityNetTests/UnitTests.cs
+++ SourceCode/MusicUtility/MusicUtilityNetTests/UnitTests.cs
@@ -6,7 +6,9 @@
 
 using MusicUtility;
 using NUnit.Framework;
+using System;
 using System.IO;
+using System.Reflection;
 
 namespace MusicUtility.Tests
 {
@@ -39,6 +41,36 @@ namespace MusicUtility.Tests
 			string location = musicUtility.ITunesLibraryLocation;
 
 			Assert.IsNotEmpty(location);
+		}
+
+		[Test]
+		public void RunRuleBasic()
+		{
+			Rule rule = new Rule();
+			rule.Subject = "MusicUtility.Tags.Album";
+			rule.Condition = Condition.ContainsRegex;
+			rule.Conditional = @"\s*\(Disc.*?\)";
+			rule.Operation = Operations.Remove;
+
+			string file = @"C:\Users\JamesMc\Data\External\Entertainment\" +
+				@"Music\6ix\What It Is! Funky Soul And Rare Grooves\" +
+				"Im Just Like You.mp3";
+			Tags tag = new Tags(file);
+
+			Type itemType = tag.GetType();
+			PropertyInfo propertyInfo =
+				itemType.GetProperty("Album");
+
+			object source = propertyInfo.GetValue(tag, null);
+
+			object result = Rules.RunRule(
+				rule, tag, "MusicUtility.Tags.Album", source, null);
+
+			string test = (string)result;
+
+			//Assert.IsNotEmpty(rule);
+			Assert.That(test, Is.EqualTo(
+				"What It Is! Funky Soul And Rare Grooves"));
 		}
 	}
 }
