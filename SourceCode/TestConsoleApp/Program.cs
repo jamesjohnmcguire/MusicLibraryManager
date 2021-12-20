2a65d2d179391ec5c32da56db59c61ddc1306a27
JamesMc <jamesjohnmcguire@gmail.com>
2021-12-20 16:45:02 +0900

    Add TestConsoleApp and clean up solution file

---
+++ SourceCode/TestConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="Program.cs" company="James John McGuire">
+// Copyright © 2021 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+[assembly: CLSCompliant(true)]
+
+namespace Music
+{
+	/// <summary>
+	/// Dbx to pst program class.
+	/// </summary>
+	public static class Program
+	{
+		/// <summary>
+		/// The program's main entry point.
+		/// </summary>
+		/// <param name="arguments">The arguments given to the program.</param>
+		/// <returns>A value indicating success or not.</returns>
+		public static int Main(string[] arguments)
+		{
+			int result = -1;
+
+			try
+			{
+			}
+			catch (Exception exception)
+			{
+				throw;
+			}
+
+			return result;
+		}
+	}
+}
