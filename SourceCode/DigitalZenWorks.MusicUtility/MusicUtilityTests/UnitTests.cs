deb6f34cfbb0f87c70657e0e9c0c20c64a409aff
James John McGuire <jamesjohnmcguire@gmail.com>
2022-02-19 18:31:08 +0900

    Prefix assembly with DigitalZenWorks.

--- SourceCode/DigitalZenWorks.MusicUtility/MusicUtilityTests/UnitTests.cs
+++ SourceCode/DigitalZenWorks.MusicUtility/MusicUtilityTests/UnitTests.cs
@@ -175,7 +175,7 @@ namespace DigitalZenWorks.MusicUtility.Tests
 		[Test]
 		public void GetDefaultRules()
 		{
-			string resourceName = "MusicUtility.DefaultRules.json";
+			string resourceName = "DigitalZenWorks.MusicUtility.DefaultRules.json";
 			Assembly assembly = typeof(MusicManager).Assembly;
 
 			using Stream templateObjectStream =
