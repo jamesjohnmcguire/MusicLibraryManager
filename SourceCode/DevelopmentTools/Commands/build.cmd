e60fa53d53bc7610b454a5b73048758c71db94a1
James John McGuire <jamesjohnmcguire@gmail.com>
2022-02-20 17:05:24 +0900

    Switch to 2 option build command

--- SourceCode/DevelopmentTools/Commands/build.cmd
+++ SourceCode/DevelopmentTools/Commands/build.cmd
@@ -1,3 +1,7 @@
+IF "%1"=="msbuild" GOTO msbuild
+GOTO default
+
+:msubuild
 @ECHO OFF
 @ECHO usage: Build ^<target type (BuildAll, Test, Publish)^> ^<Configuration (Debug, Release)^> ^<build type (Rebuild, Build, Clean)^>
 @ECHO defaults: Rebuild Release
@@ -21,3 +25,26 @@ IF [%3]==[] SET buildType=Rebuild
 @ECHO buildType: %buildType%
 
 msbuild MusicLibrary.msbuild.xml /p:BuildType=%buildType%;Configuration=%configuration%;SolutionPath=..\..;Platform=x64 /t:%target% -restore
+
+:default
+CD %~dp0
+CD ..\..
+
+REM IF "%1"=="release" CALL VersionUpdate BackUpManagerLibrary\BackupManagerLibrary.csproj
+REM IF "%1"=="release" CALL VersionUpdate BackUpManager\BackupManager.csproj
+
+IF EXIST Bin\Release\AnyCPU\NUL DEL /Q Bin\Release\AnyCPU\*.*
+
+CALL msbuild -property:Configuration=Release;Platform="Any CPU"
+
+IF "%1"=="release" GOTO release
+GOTO end
+
+:release
+CD Bin\Release\AnyCPU
+
+7z u MusicManager.zip .
+
+hub release create -a MusicManager.zip -m "%2" v%2
+
+:end
