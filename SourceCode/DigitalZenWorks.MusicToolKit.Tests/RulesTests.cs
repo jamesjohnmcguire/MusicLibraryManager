3947a5102010d609fc9e5a8a446a7d2126a13030
James John McGuire <jamesjohnmcguire@gmail.com>
2022-10-03 19:11:10 +0900

    Add RulesTest class

---
+++ SourceCode/DigitalZenWorks.MusicToolKit.Tests/RulesTests.cs
@@ -0,0 +1,43 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="RulesTests.cs" company="Digital Zen Works">
+// Copyright © 2019 - 2022 Digital Zen Works. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using DigitalZenWorks.Common.Utilities;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+using CommonLogging = Common.Logging;
+
+namespace DigitalZenWorks.MusicToolKit.Tests
+{
+	/// <summary>
+	/// Rules tests class.
+	/// </summary>
+	[TestFixture]
+	public class RulesTests
+	{
+		/// <summary>
+		/// The one time setup method.
+		/// </summary>
+		[SetUp]
+		public void OneTimeSetUp()
+		{
+		}
+
+		/// <summary>
+		/// One time tear down method.
+		/// </summary>
+		[OneTimeTearDown]
+		public void OneTimeTearDown()
+		{
+		}
+	}
+}
