157c9d15aa460abaff07b5c6ad9829cb7faf1113
James John McGuire <jamesjohnmcguire@gmail.com>
2022-10-03 22:22:00 +0900

    Add iTunes test class

---
+++ SourceCode/DigitalZenWorks.MusicToolKit.Tests/ItunesTests.cs
@@ -0,0 +1,20 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="ItunesTests.cs" company="Digital Zen Works">
+// Copyright © 2019 - 2022 Digital Zen Works. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using iTunesLib;
+using NUnit.Framework;
+using System;
+
+namespace DigitalZenWorks.MusicToolKit.Tests
+{
+	/// <summary>
+	/// iTunes tests class.
+	/// </summary>
+	[TestFixture]
+	public class ItunesTests : TestsBase
+	{
+	}
+}
