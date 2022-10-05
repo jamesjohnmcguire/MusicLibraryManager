f57088271558f313457170a274f3431603fcdf33
James John McGuire <jamesjohnmcguire@gmail.com>
2022-10-05 13:42:37 +0900

    Add ITunesManager class

---
+++ SourceCode/DigitalZenWorks.MusicToolKit/ITunesManager.cs
@@ -0,0 +1,17 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="ITunesManager.cs" company="Digital Zen Works">
+// Copyright © 2019 - 2022 Digital Zen Works. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+namespace DigitalZenWorks.MusicToolKit
+{
+	/// <summary>
+	/// Manages iTunes operations class.
+	/// </summary>
+	public class ITunesManager
+	{
+	}
+}
