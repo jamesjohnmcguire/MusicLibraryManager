c4a8272b8e5a61e6e470205d8107b46e7c5dd7f2
James John McGuire <jamesjohnmcguire@gmail.com>
2022-11-04 00:59:47 +0900

    Add Tag Rule Classes

---
+++ SourceCode/DigitalZenWorks.MusicToolKit/AlbumTagRules.cs
@@ -0,0 +1,18 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="AlbumTagRules.cs" company="Digital Zen Works">
+// Copyright © 2019 - 2022 Digital Zen Works. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Text;
+
+namespace DigitalZenWorks.MusicToolKit
+{
+	/// <summary>
+	/// Album tag rules class.
+	/// </summary>
+	public class AlbumTagRules
+	{
+	}
+}
---
+++ SourceCode/DigitalZenWorks.MusicToolKit/TagRules.cs
@@ -0,0 +1,18 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="TagRules.cs" company="Digital Zen Works">
+// Copyright © 2019 - 2022 Digital Zen Works. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Text;
+
+namespace DigitalZenWorks.MusicToolKit
+{
+	/// <summary>
+	/// General Tag Rules.
+	/// </summary>
+	public static class TagRules
+	{
+	}
+}
