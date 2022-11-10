55c258a11f3d49ff624cd6459d97012ffa28427e
James John McGuire <jamesjohnmcguire@gmail.com>
2022-11-10 19:25:12 +0900

    Add ArtistTagRules Class

---
+++ SourceCode/DigitalZenWorks.MusicToolKit/ArtistTagRules.cs
@@ -0,0 +1,17 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="ArtistTagRules.cs" company="Digital Zen Works">
+// Copyright © 2019 - 2022 Digital Zen Works. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using DigitalZenWorks.RulesLibrary;
+
+namespace DigitalZenWorks.MusicToolKit
+{
+	/// <summary>
+	/// Artist tag rules class.
+	/// </summary>
+	public static class ArtistTagRules
+	{
+	}
+}
