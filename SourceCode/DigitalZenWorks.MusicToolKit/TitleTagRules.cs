9c3a96c44a7a379dfb1e6967855e0624e2dde6d0
James John McGuire <jamesjohnmcguire@gmail.com>
2022-11-11 19:29:24 +0900

    Add TitleTagRules Class

---
+++ SourceCode/DigitalZenWorks.MusicToolKit/TitleTagRules.cs
@@ -0,0 +1,15 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="TitleTagRules.cs" company="Digital Zen Works">
+// Copyright © 2019 - 2022 Digital Zen Works. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+namespace DigitalZenWorks.MusicToolKit
+{
+	/// <summary>
+	/// Title tag rules class.
+	/// </summary>
+	public class TitleTagRules
+	{
+	}
+}
