811a5c5b4c4be70f1d10e25456d9fefab147bbe9
James John McGuire <jamesjohnmcguire@gmail.com>
2022-11-15 18:39:50 +0900

    Add Initial Decoders Support

---
+++ SourceCode/DigitalZenWorks.MusicToolKit/Decoders/AudioDecoder.cs
@@ -0,0 +1,87 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="AudioDecoder.cs" company="Digital Zen Works">
+// Copyright © 2019 - 2022 Digital Zen Works. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+namespace DigitalZenWorks.MusicToolKit
+{
+	/// <summary>
+	/// Abstract base class for audio decoders.
+	/// </summary>
+	public abstract class AudioDecoder : IAudioDecoder
+	{
+		/// <summary>
+		/// Buffer size field.
+		/// </summary>
+		protected static readonly int BUFFERSIZE = 2 * 192000;
+
+		/// <summary>
+		/// The sample rate.
+		/// </summary>
+		protected int sampleRate;
+
+		/// <summary>
+		/// The channels property.
+		/// </summary>
+		protected int channels;
+
+		/// <summary>
+		/// Finalizes an instance of the <see cref="AudioDecoder"/> class.
+		/// Destructor.
+		/// </summary>
+		~AudioDecoder() => Dispose(false);
+
+		/// <summary>
+		/// Gets the sample rate.
+		/// </summary>
+		/// <value>The sample rate.</value>
+		public int SampleRate
+		{
+			get { return sampleRate; }
+		}
+
+		/// <summary>
+		/// Gets the channels property.
+		/// </summary>
+		/// <value>The channels property.v</value>
+		public int Channels
+		{
+			get { return channels; }
+		}
+
+		/// <summary>
+		/// Gets or sets the format property.
+		/// </summary>
+		/// <value>The format property.</value>
+		public AudioProperties Format { get; protected set; }
+
+		/// <summary>
+		/// The decode method.
+		/// </summary>
+		/// <param name="consumer">The consumer.</param>
+		/// <param name="maxLength">The maximum length.</param>
+		/// <returns>A valude indicating success or not.</returns>
+		public abstract bool Decode(IAudioConsumer consumer, int maxLength);
+
+		/// <summary>
+		/// The dispose method.
+		/// </summary>
+		public virtual void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		/// <summary>
+		/// The dispose method.
+		/// </summary>
+		/// <param name="disposing">A value indicating disposing
+		/// or not.</param>
+		protected virtual void Dispose(bool disposing)
+		{
+		}
+	}
+}
---
+++ SourceCode/DigitalZenWorks.MusicToolKit/Decoders/NAudioDecoder.cs
@@ -0,0 +1,127 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="NAudioDecoder.cs" company="Digital Zen Works">
+// Copyright © 2019 - 2022 Digital Zen Works. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+namespace DigitalZenWorks.MusicToolKit
+{
+	using NAudio.Wave;
+	using System;
+	using System.Buffers;
+	using System.IO;
+
+	/// <summary>
+	/// Decode using the NAudio library.
+	/// </summary>
+	public class NAudioDecoder : AudioDecoder
+	{
+		private readonly string file;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="NAudioDecoder"/> class.
+		/// </summary>
+		/// <param name="file">The file to decode.</param>
+		public NAudioDecoder(string file)
+		{
+			this.file = file;
+
+			Initialize();
+		}
+
+		/// <summary>
+		/// The decode method.
+		/// </summary>
+		/// <param name="consumer">The consumer.</param>
+		/// <param name="maxLength">The maximum length.</param>
+		/// <returns>A value indicating success or not.</returns>
+		public override bool Decode(IAudioConsumer consumer, int maxLength)
+		{
+			bool result = false;
+
+			if (consumer != null)
+			{
+				using WaveStream reader = OpenWaveStream(file);
+
+				if (reader.WaveFormat.BitsPerSample != 16)
+				{
+					return false;
+				}
+
+				int remaining, length, size;
+
+				var buffer = ArrayPool<byte>.Shared.Rent(2 * BUFFERSIZE);
+				var data = ArrayPool<short>.Shared.Rent(BUFFERSIZE);
+
+				// Samples to read to get maxLength seconds of audio
+				remaining = maxLength * this.Format.Channels * this.sampleRate;
+
+				// Bytes to read
+				length = 2 * Math.Min(remaining, BUFFERSIZE);
+
+				while ((size = reader.Read(buffer, 0, length)) > 0)
+				{
+					Buffer.BlockCopy(buffer, 0, data, 0, size);
+
+					consumer.Consume(data, size / 2);
+
+					remaining -= size / 2;
+					if (remaining <= 0)
+					{
+						break;
+					}
+
+					length = 2 * Math.Min(remaining, BUFFERSIZE);
+				}
+
+				ArrayPool<byte>.Shared.Return(buffer);
+				ArrayPool<short>.Shared.Return(data);
+
+				result = true;
+			}
+
+			return result;
+		}
+
+		private static WaveStream OpenWaveStream(string file)
+		{
+			WaveStream waveStream;
+
+			string extension = Path.GetExtension(file);
+			extension = extension.ToUpperInvariant();
+
+			switch (extension)
+			{
+				case ".MP3":
+					waveStream = new Mp3FileReader(file);
+					break;
+				case ".WAV":
+					waveStream = new WaveFileReader(file);
+					break;
+				default:
+					waveStream = new MediaFoundationReader(file);
+					break;
+			}
+
+			return waveStream;
+		}
+
+		private bool Initialize()
+		{
+			using WaveStream reader = OpenWaveStream(file);
+
+			var format = reader.WaveFormat;
+
+			this.sampleRate = format.SampleRate;
+			this.channels = format.Channels;
+
+			this.Format = new AudioProperties(
+				format.SampleRate,
+				format.BitsPerSample,
+				format.Channels,
+				(int)reader.TotalTime.TotalSeconds);
+
+			return format.BitsPerSample != 16;
+		}
+	}
+}
