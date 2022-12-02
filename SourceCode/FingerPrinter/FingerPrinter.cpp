dcb0a92a5be3b0dad0ba5ad3f46386e3c3f2ba32
James John McGuire <jamesjohnmcguire@gmail.com>
2022-12-02 14:41:35 +0900

    Add New Cmake Project Files

---
+++ SourceCode/FingerPrinter/FingerPrinter.cpp
@@ -0,0 +1,12 @@
+﻿// FingerPrinter.cpp : Defines the entry point for the application.
+//
+
+#include "FingerPrinter.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
