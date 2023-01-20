783625a70f3f9bb3b1b866e7af4493c5cd3a39d2
James John McGuire <jamesjohnmcguire@gmail.com>
2023-01-20 16:46:19 +0900

    Add Initial Test Stubs

--- SourceCode/FingerPrinter.Tests/test.cpp
+++ SourceCode/FingerPrinter.Tests/test.cpp
@@ -1,6 +1,17 @@
 #include "pch.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(TestCaseName, TestName)
+{
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
+
+TEST(TestFingerPrinter, Success)
+{
+	EXPECT_TRUE(true);
+}
+
+TEST(TestFingerPrinter, Correct)
+{
+	EXPECT_EQ(1, 1);
 }
