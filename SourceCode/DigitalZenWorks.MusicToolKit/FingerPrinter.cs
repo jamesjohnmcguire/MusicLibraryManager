69ae6e69f55c43c3be1885be48567ed862fe4a72
James John McGuire <jamesjohnmcguire@gmail.com>
2023-01-23 13:42:37 +0900

    Add FingerPrinter Class

---
+++ SourceCode/DigitalZenWorks.MusicToolKit/FingerPrinter.cs
@@ -0,0 +1,15 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="FingerPrinter.cs" company="Digital Zen Works">
+// Copyright © 2019 - 2023 Digital Zen Works. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+namespace DigitalZenWorks.MusicToolKit
+{
+	/// <summary>
+	/// Represents an audio finger print signature.
+	/// </summary>
+	public class FingerPrinter
+	{
+	}
+}
