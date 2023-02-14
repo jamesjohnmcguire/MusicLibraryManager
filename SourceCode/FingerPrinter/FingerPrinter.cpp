18580fbaf9d3dc4e6e8bb87e9bf7790a2b00258f
James John McGuire <jamesjohnmcguire@gmail.com>
2023-02-14 15:38:56 +0900

    Add Beginnings of FingerPrint Function

--- SourceCode/FingerPrinter/FingerPrinter.cpp
+++ SourceCode/FingerPrinter/FingerPrinter.cpp
@@ -1,5 +1,14 @@
-﻿// FingerPrinter.cpp : Defines the entry point for the application.
-//
+﻿#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <chrono>
+
+#define USE_SWRESAMPLE
+#include "..\ChromaPrint\src\chromaprint.h"
+#include "..\ChromaPrint\src/audio/ffmpeg_audio_reader.h"
+
+using namespace chromaprint;
 
 #include <iostream>
 
@@ -11,6 +20,19 @@ namespace FingerPrinter
 	{
 		char* result = nullptr;
 
+		FFmpegAudioReader reader;
+
+		ChromaprintContext* chromaprint_ctx =
+			chromaprint_new(CHROMAPRINT_ALGORITHM_DEFAULT);
+
+		auto channels = chromaprint_get_num_channels(chromaprint_ctx);
+		auto sampleRate = chromaprint_get_sample_rate(chromaprint_ctx);
+		reader.SetOutputChannels(channels);
+		reader.SetOutputSampleRate(sampleRate);
+
+		//ProcessFile(chromaprint_ctx, reader, argv[i]);
+
+		chromaprint_free(chromaprint_ctx);
 		return result;
 	}
 }
