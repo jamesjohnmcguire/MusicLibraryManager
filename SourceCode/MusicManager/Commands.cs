8d944a58d9e0ba6509144d92e47b199a294f09b6
James John McGuire <jamesjohnmcguire@gmail.com>
2024-01-04 13:11:54 +0900

    Move GetCommands to Commands

--- SourceCode/MusicManager/Commands.cs
+++ SourceCode/MusicManager/Commands.cs
@@ -4,15 +4,44 @@
 // </copyright>
 /////////////////////////////////////////////////////////////////////////////
 
+using DigitalZenWorks.CommandLine.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MusicManager
+namespace DigitalZenWorks.Music.ToolKit.Application
 {
 	internal static class Commands
 	{
+		public static IList<Command> GetCommands()
+		{
+			IList<Command> commands = new List<Command>();
+
+			Command help = new("help");
+			help.Description = "Show this information";
+			commands.Add(help);
+
+			IList<CommandOption> options = new List<CommandOption>();
+
+			CommandOption location = new("l", "location", true);
+			options.Add(location);
+
+			Command extractTags =
+				new("extract-tags", options, 0, "Extract Tags Information");
+			commands.Add(extractTags);
+
+			CommandOption rules = new("r", "rules", true);
+			options.Add(rules);
+
+			CommandOption noUpdateTags = new("n", "no-update-tags", false);
+			options.Add(noUpdateTags);
+
+			Command clean = new("clean", options, 0, "Clean music files");
+			commands.Add(clean);
+
+			return commands;
+		}
 	}
 }
--- SourceCode/MusicManager/Program.cs
+++ SourceCode/MusicManager/Program.cs
@@ -42,7 +42,7 @@ namespace DigitalZenWorks.Music.ToolKit.Application
 
 				Log.Info("Starting Music Manager");
 
-				IList<Command> commands = GetCommands();
+				IList<Command> commands = Commands.GetCommands();
 
 				CommandLineArguments commandLine = new (commands, arguments);
 
@@ -113,35 +113,6 @@ namespace DigitalZenWorks.Music.ToolKit.Application
 
 				throw;
 			}
-		}
-
-		private static IList<Command> GetCommands()
-		{
-			IList<Command> commands = new List<Command>();
-
-			Command help = new ("help");
-			help.Description = "Show this information";
-			commands.Add(help);
-
-			IList<CommandOption> options = new List<CommandOption>();
-
-			CommandOption location = new ("l", "location", true);
-			options.Add(location);
-
-			Command extractTags =
-				new ("extract-tags", options, 0, "Extract Tags Information");
-			commands.Add(extractTags);
-
-			CommandOption rules = new ("r", "rules", true);
-			options.Add(rules);
-
-			CommandOption noUpdateTags = new ("n", "no-update-tags", false);
-			options.Add(noUpdateTags);
-
-			Command clean = new ("clean", options, 0, "Clean music files");
-			commands.Add(clean);
-
-			return commands;
 		}
 
 		private static string GetLocation(Command command)
