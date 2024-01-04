d14065aa592d060ddd73ebc598c1fe2faff8ba4c
James John McGuire <jamesjohnmcguire@gmail.com>
2024-01-04 13:08:44 +0900

    Add Commands Class

---
+++ SourceCode/MusicManager/Commands.cs
@@ -0,0 +1,18 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="Commands.cs" company="Digital Zen Works">
+// Copyright © 2019 - 2024 Digital Zen Works. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicManager
+{
+	internal static class Commands
+	{
+	}
+}
