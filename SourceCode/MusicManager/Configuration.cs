15a02acd4624098cf70a28ba709b5fa708867c1e
James John McGuire <jamesjohnmcguire@gmail.com>
2024-01-04 13:14:51 +0900

    Add Configuration Class

---
+++ SourceCode/MusicManager/Configuration.cs
@@ -0,0 +1,12 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="Configuration.cs" company="Digital Zen Works">
+// Copyright © 2019 - 2024 Digital Zen Works. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+namespace DigitalZenWorks.Music.ToolKit.Application
+{
+	internal class Configuration
+	{
+	}
+}
