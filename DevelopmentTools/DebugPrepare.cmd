d2f7c2328b000e6a22331443338d179c669c286a
James John McGuire <jamesjohnmcguire@gmail.com>
2025-03-20 19:38:55 +0900

    Fix Regex

--- DevelopmentTools/DebugPrepare.cmd
+++ DevelopmentTools/DebugPrepare.cmd
@@ -5,7 +5,7 @@ IF "%1"=="clean" GOTO clean
 IF "%1"=="extract" GOTO extract
 
 :clean
-sed -i "s|[A-Za-z0-9_.]* C:|clean C:|g" launchSettings.json
+sed -i "s|[A-Za-z0-9_.-]* C:|clean C:|g" launchSettings.json
 GOTO finish
 
 :extract
