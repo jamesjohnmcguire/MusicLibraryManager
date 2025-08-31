88ecbd5f9d1d278617e63e34abaad5f1965ec48b
James John McGuire <jamesjohnmcguire@gmail.com>
2025-08-31 23:27:17 +0900

    Further Enhancements

--- DevelopmentTools/compare_audio_detailed.bat
+++ DevelopmentTools/compare_audio_detailed.bat
@@ -5,17 +5,26 @@ SETLOCAL EnableDelayedExpansion
 if "%~2"=="" SET Message=Usage: <<media file1>>  <<media file2>>
 if "%~2"=="" GOTO error
 
+SET "TempDirectory=%TEMP%\AudioCompare%RANDOM%"
+MKDIR "%TempDirectory%"
+
 :: Store the file names without extension for working files
 SET "File1Base=%~n1"
 SET "File2Base=%~n2"
-SET File1="%TEMP%\%File1Base%.Metadata.txt"
-SET File2="%TEMP%\%File2Base%.Metadata.txt"
+SET File1="%TempDirectory%\%File1Base%.Metadata.txt"
+SET File2="%TempDirectory%\%File2Base%.Metadata.txt"
+SET DiffFile="%empDirectory%\metadata_diff.txt"
 
-REM ... [previous code] ...
+ECHO Comparing files:
+ECHO 1: %~1
+ECHO 2: %~2
+ECHO.
 
 ECHO Extracting detailed metadata...
 ECHO Full metadata for %~1: > "%File1%"
+:: ffmpeg -i "%~1" -f ffmetadata "%temp_dir%\%file1_base%_metadata.txt" 2>nul
 ffmpeg -i "%~1" 2>> "%File1%"
+
 ECHO Additional technical details: >> "%File1%"
 ffprobe -show_format -show_streams "%~1" 2>> "%File1%"
 
@@ -25,6 +34,14 @@ ECHO Additional technical details: >> "%File2%"
 ffprobe -show_format -show_streams "%~2" 2>> "%File2%"
 
 REM ... [rest of code] ...
+
+ECHO Comparing metadata...
+fc "%File1%" "%File2%" > "%DiffFile%"
+if errorlevel 1 SET Message=Metadata differences found - see "%DiffFile%"
+if errorlevel 0 SET Message=No metadata differences found
+
+ECHO %Message%
+ECHO.
 
 ;error
 ECHO %Message%
