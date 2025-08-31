3af6215a2a3181de69af5627c5a2d82a8585da04
James John McGuire <jamesjohnmcguire@gmail.com>
2025-08-31 22:47:16 +0900

    Clean Up Script

--- DevelopmentTools/compare_audio_detailed.bat
+++ DevelopmentTools/compare_audio_detailed.bat
@@ -1,17 +1,33 @@
-@echo off
-setlocal EnableDelayedExpansion
+@ECHO off
+SETLOCAL EnableDelayedExpansion
+
+
+if "%~2"=="" SET Message=Usage: <<media file1>>  <<media file2>>
+if "%~2"=="" GOTO error
+
+:: Store the file names without extension for working files
+SET "File1Base=%~n1"
+SET "File2Base=%~n2"
+SET File1="%TEMP%\%File1Base%.Metadata.txt"
+SET File2="%TEMP%\%File2Base%.Metadata.txt"
 
 REM ... [previous code] ...
 
-echo Extracting detailed metadata...
-echo Full metadata for %~1: > "%temp_dir%\%file1_base%_full_metadata.txt"
-ffmpeg -i "%~1" 2>> "%temp_dir%\%file1_base%_full_metadata.txt"
-echo Additional technical details: >> "%temp_dir%\%file1_base%_full_metadata.txt"
-ffprobe -show_format -show_streams "%~1" 2>> "%temp_dir%\%file1_base%_full_metadata.txt"
+ECHO Extracting detailed metadata...
+ECHO Full metadata for %~1: > "%File1%"
+ffmpeg -i "%~1" 2>> "%File1%"
+ECHO Additional technical details: >> "%File1%"
+ffprobe -show_format -show_streams "%~1" 2>> "%File1%"
 
-echo Full metadata for %~2: > "%temp_dir%\%file2_base%_full_metadata.txt"
-ffmpeg -i "%~2" 2>> "%temp_dir%\%file2_base%_full_metadata.txt"
-echo Additional technical details: >> "%temp_dir%\%file2_base%_full_metadata.txt"
-ffprobe -show_format -show_streams "%~2" 2>> "%temp_dir%\%file2_base%_full_metadata.txt"
+ECHO Full metadata for %~2: > "%File2%"
+ffmpeg -i "%~2" 2>> "%File2%"
+ECHO Additional technical details: >> "%File2%"
+ffprobe -show_format -show_streams "%~2" 2>> "%File2%"
 
 REM ... [rest of code] ...
+
+;error
+ECHO %Message%
+EXIT /b 1
+
+ENDLOCAL
