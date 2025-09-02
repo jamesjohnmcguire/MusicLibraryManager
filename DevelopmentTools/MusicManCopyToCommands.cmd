e83205906d3b5d494be324b9360f6744b0615ef2
James John McGuire <jamesjohnmcguire@gmail.com>
2025-09-02 11:36:07 +0900

    Remove Poorly Placed File

--- DevelopmentTools/MusicManCopyToCommands.cmd
+++
@@ -1,3 +0,0 @@
-SET utils=%USERPROFILE%\Data\Commands
-SET source=%USERPROFILE%\Data\Clients\DigitalZenWorks\MusicLibraryManager\SourceCode\MusicManager\Bin\Release\publish
-XCOPY /D /E /H /I /R /S /Y %source% %utils%
