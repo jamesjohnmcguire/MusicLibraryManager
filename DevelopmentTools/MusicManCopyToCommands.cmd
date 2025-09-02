95887e8907fc8cd67050e7761ef2cec114256440
James John McGuire <jamesjohnmcguire@gmail.com>
2025-09-02 11:26:09 +0900

    Clean Up Scripts

--- DevelopmentTools/MusicManCopyToCommands.cmd
+++ DevelopmentTools/MusicManCopyToCommands.cmd
@@ -1,3 +1,3 @@
 SET utils=%USERPROFILE%\Data\Commands
 SET source=%USERPROFILE%\Data\Clients\DigitalZenWorks\MusicLibraryManager\SourceCode\MusicManager\Bin\Release\publish
-xcopy /D /E /H /I /R /S /Y %source% %utils%
+XCOPY /D /E /H /I /R /S /Y %source% %utils%
