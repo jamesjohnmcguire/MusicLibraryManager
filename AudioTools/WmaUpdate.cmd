706b21a7539949416c6fc9cc58da0e42b6577a84
James John McGuire <jamesjohnmcguire@gmail.com>
2026-01-06 19:04:02 +0900

    Move Scripts to AudioTools

--- AudioTools/WmaUpdate.cmd
+++ AudioTools/WmaUpdate.cmd
@@ -1,10 +1,10 @@
-@echo off
-setlocal
+@ECHO OFF
+SETLOCAL
 
 REM Check if the input file is provided
 if "%~1"=="" (
-    echo Usage: convert_wma_to_m4a input_file.wma
-    exit /b 1
+	echo Usage: WmaUpdate input_file.wma
+	exit /b 1
 )
 
 REM Extract the file name without extension
@@ -14,10 +14,12 @@ REM Extract the source bitrate using ffprobe
 for /f "tokens=*" %%i in ('ffprobe -v error -select_streams a:0 -show_entries stream=bit_rate -of default=nokey=1:noprint_wrappers=1 "%~1"') do set BitRate=%%i
 
 REM If bitrate extraction failed, set a default bitrate
+if "%BitRate%"=="" ECHO Unable to obtain bitrate, setting to 192K
 if "%BitRate%"=="" set BitRate=192k
+PAUSE
 
 REM Convert the WMA file to M4A using the extracted bitrate
 ffmpeg -i "%~1" -c:a aac -b:a %BitRate% "%FileName%.m4a"
 
 echo Conversion complete: %FileName%.m4a
-endlocal
+ENDLOCAL
