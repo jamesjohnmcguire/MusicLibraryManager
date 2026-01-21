78016d9a2fc75779fb19a3142bb9d441cb7ade7f
James John McGuire <jamesjohnmcguire@gmail.com>
2026-01-21 20:10:42 +0900

    Move Additional Functionality to Base Test Class

--- SourceCode/DigitalZenWorks.MusicToolKit.Tests/BaseTestsSupport.cs
+++ SourceCode/DigitalZenWorks.MusicToolKit.Tests/BaseTestsSupport.cs
@@ -7,9 +7,11 @@
 namespace DigitalZenWorks.MusicToolKit.Tests;
 
 using System;
+using System.Collections.Generic;
 using System.IO;
 using DigitalZenWorks.Common.Utilities;
 using DigitalZenWorks.RulesLibrary;
+using MediaFileToolkit;
 using NUnit.Framework;
 
 /// <summary>
@@ -17,6 +19,7 @@ using NUnit.Framework;
 /// </summary>
 internal class BaseTestsSupport
 {
+	private readonly AudioSettings audioSettings = new();
 	private Rules rules;
 	private string temporaryPath;
 	private string testFile;
@@ -38,6 +41,8 @@ internal class BaseTestsSupport
 	/// </summary>
 	/// <value>The test file.</value>
 	public string TestFile { get => testFile; }
+
+	public Dictionary<string, string> TestFiles;
 
 	/// <summary>
 	/// The one time setup method.
@@ -67,6 +72,32 @@ internal class BaseTestsSupport
 		{
 			Directory.Delete(temporaryPath, true);
 		}
+	}
+
+	protected string? GenerateAudioFile(string format)
+	{
+		string? audioFile = null;
+		string outputPath = Path.Combine(TemporaryPath, $"test.{format}");
+
+		string arguments = GetArguments(format, outputPath);
+
+		ExternalProcess process = new();
+
+		bool result = process.Execute("ffmpeg", arguments);
+		bool exists = File.Exists(outputPath);
+
+		Assert.Multiple(() =>
+		{
+			Assert.That(result, Is.True);
+			Assert.That(exists, Is.True);
+		});
+
+		if (result == true && exists == true)
+		{
+			audioFile = outputPath;
+		}
+
+		return audioFile;
 	}
 
 	/// <summary>
@@ -102,5 +133,38 @@ internal class BaseTestsSupport
 		Directory.CreateDirectory(uniqueDirectoryPath);
 
 		return uniqueDirectoryPath;
+	}
+
+	private string GetArguments(string format, string outputPath)
+	{
+		string baseArguments = "-f lavfi -i " +
+			$"\"sine=frequency={audioSettings.Frequency}:" +
+			$"duration={audioSettings.Duration}\"";
+
+		string? codec = format switch
+		{
+			"aac" => "-codec:a aac -b:a 32k",
+			"aiff" => "-codec:a pcm_s16be",
+
+			// APE not available, not supported yet
+			// "ape" => "-codec:a ape -compression_level 2000",
+			// Use FLAC as lossless alternative
+			// "ape" => "-codec:a flac -compression_level 5",
+			"flac" => "-codec:a flac",
+			"m4a" => "-codec:a aac -b:a 32k",
+			"mka" => "-codec:a libvorbis -b:a 32k",
+			"mp3" => "-codec:a libmp3lame -b:a 32k",
+			"ogg" => "-codec:a libvorbis -b:a 32k",
+			"opus" => "-codec:a libopus -b:a 32k",
+			"tta" => "-codec:a tta",
+			"wav" => "-codec:a pcm_s16le",
+			"wma" => "-codec:a wmav2 -b:a 32k",
+			"wv" => "-codec:a wavpack",
+			_ => null
+		};
+
+		string arguments = $"{baseArguments} {codec} \"{outputPath}\"";
+
+		return arguments;
 	}
 }
